e0f16c2a18eed644ad838f2b43a4be1020cf235c
dcabulo <diego.cabulo@linktic.co>
2022-04-19 22:05:28 -0500

    feat: new variables for test Page tags

--- src/TestPage.java
+++ src/TestPage.java
@@ -3,4 +3,10 @@ public class TestPage {
     String thirdListElementInsideUnOrderList = "ul li:nth-child(3)";
     String firstListElementInsideMenuTag = "menu li:nth-child(1)";
     String thirdListElementInsideOrderedList = "ol li:nth-child(3)";
+    String buttonCoolText = "#but";
+    String buttonResetText2 = "#f01";
+    String thirdOptionInSelect = "#f11 option:nth-child(3)";
+    String fourRowThirdColumn = "table[summary] tr:nth-child(4) td:nth-child(3)";
+    String pTagBelowH2Tag = "p:nth-of-type(7)";
+    String h3TagTextJavascript = "h3:nth-of-type(2)";
 }
