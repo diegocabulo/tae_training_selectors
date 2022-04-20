142c69e3363007404241ca9e13582da4a7aa8d11
dcabulo <diego.cabulo@linktic.co>
2022-04-19 22:31:07 -0500

    feat: facebook selectors

---
+++ src/FacebookSelectors.java
@@ -0,0 +1,5 @@
+public class FacebookSelectors {
+    String loginButton = "button[name=login]";
+    String anchorTagMyProfile = "a[href='/me/']";
+    String anchorTagMyFriends = "a[href='https://www.facebook.com/friends/";
+}
