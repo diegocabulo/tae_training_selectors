2a25c7ec11d615808f4f71d80f64e574ed95026a
dcabulo <diego.cabulo@linktic.co>
2022-04-19 21:39:36 -0500

    initial commit

---
+++ src/TestPage.java
@@ -0,0 +1,6 @@
+public class TestPage {
+    String anchorTagInAddress = "address a:nth-child(1)";
+    String thirdListElementInsideUnOrderList = "ul li:nth-child(3)";
+    String firstListElementInsideMenuTag = "menu li:nth-child(1)";
+    String thirdListElementInsideOrderedList = "ol li:nth-child(3)";
+}
