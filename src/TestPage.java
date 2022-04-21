1a8247905818c71e9bf60cb708986e461f445042
dcabulo <diego.cabulo@linktic.co>
2022-04-20 20:33:24 -0500

    feat: selectors for java selenium

--- src/TestPage.java
+++ src/TestPage.java
@@ -1,12 +1,12 @@
 public class TestPage {
-    String anchorTagInAddress = "address a:nth-child(1)";
-    String thirdListElementInsideUnOrderList = "ul li:nth-child(3)";
-    String firstListElementInsideMenuTag = "menu li:nth-child(1)";
-    String thirdListElementInsideOrderedList = "ol li:nth-child(3)";
-    String buttonCoolText = "#but";
-    String buttonResetText2 = "#f01";
-    String thirdOptionInSelect = "#f11 option:nth-child(3)";
-    String fourRowThirdColumn = "table[summary] tr:nth-child(4) td:nth-child(3)";
-    String pTagBelowH2Tag = "p:nth-of-type(7)";
-    String h3TagTextJavascript = "h3:nth-of-type(2)";
+    String anchorTagInAddress = "By.cssSelector(address a:nth-child(1))";
+    String thirdListElementInsideUnOrderList = "By.cssSelector(ul li:nth-child(3))";
+    String firstListElementInsideMenuTag = "By.cssSelector(menu li:nth-child(1))";
+    String thirdListElementInsideOrderedList = "By.cssSelector(ol li:nth-child(3))";
+    String buttonCoolText = "By.id(but)";
+    String buttonResetText2 = "By.id(f01)";
+    String thirdOptionInSelect = "By.cssSelector(#f11 option:nth-child(3))";
+    String fourRowThirdColumn = "By.cssSelector(table[summary] tr:nth-child(4) td:nth-child(3))";
+    String pTagBelowH2Tag = "By.cssSelector(p:nth-of-type(7))";
+    String h3TagTextJavascript = "By.cssSelector(h3:nth-of-type(2))";
 }
